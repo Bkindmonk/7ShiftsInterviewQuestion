f9e85cc5e8eee56c7c409a4642e32732f6945f92
Bkindmonk <Bkindmonk@kryogenicamusement.ca>
2019-01-16 21:00:21 -0600

    added function stub and test stub

--- SevenShiftsInterview/SevenShiftsInterview/Program.cs
+++ SevenShiftsInterview/SevenShiftsInterview/Program.cs
@@ -5,5 +5,12 @@
         public static void Main(string[] args)
         {
         }
+        
+        private bool TestAddQuestion1(){
+            return false;
+        }
+        private int Add(string numbers){
+            return -1;
+        }
     }
 }
