ea07f02747259ada7095eb97de7d048e79307d2b
Bkindmonk <Bkindmonk@kryogenicamusement.ca>
2019-01-16 21:09:03 -0600

    implemented test for question 1

--- SevenShiftsInterview/SevenShiftsInterview/Program.cs
+++ SevenShiftsInterview/SevenShiftsInterview/Program.cs
@@ -1,15 +1,41 @@
-﻿namespace SevenShiftsInterview
+﻿using System;
+
+namespace SevenShiftsInterview
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            try
+            {
+                TestAddQuestion1();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
         
-        private bool TestAddQuestion1(){
-            return false;
+        
+        private static void TestAddQuestion1(){
+            //The numbers in the string are separated by a comma
+            bool case1 = (Add("1,2,3,4")==10);
+
+            //Empty strings should return 0
+            bool case2 = (Add("") == 0);
+            
+            //Testing multiple digits numbers
+            bool case3 = (Add("1000,500,30,9") == 1539);
+            
+            //Testing single number strings
+            bool case4 = (Add("500") == 500);
+
+            if(!case1){throw new Exception("case1 failed");}
+            if(!case2){throw new Exception("case2 failed");}
+            if(!case3){throw new Exception("case3 failed");}
+            if(!case4){throw new Exception("case4 failed");}
         }
-        private int Add(string numbers){
+        private static int Add(string numbers){
             return -1;
         }
     }
